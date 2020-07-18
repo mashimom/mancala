2c519fbbe6a70281b4586c4914e8edaa39e6ab12
Marco Shimomoto <mashimom@gmail.com>
2020-07-18 13:27:29 +0200

    improved build and added testing configs

---
+++ src/test/resources/SpockConfig.groovy
@@ -0,0 +1,8 @@
+runner {
+	optimizeRunOrder true
+	filterStackTrace true
+}
+spockReports {
+	set 'com.athaydes.spockframework.report.showCodeBlocks': true
+//	set 'com.athaydes.spockframework.report.outputDir': 'build/reports/tests/spock-reports'
+}
