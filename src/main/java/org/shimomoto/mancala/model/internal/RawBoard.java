a8c30679b6b6d6000c5340b388a6d220f0fa27da
Marco Shimomoto <mashimom@gmail.com>
2020-07-26 10:31:08 +0200

    code cleanup from inspections

--- src/main/java/org/shimomoto/mancala/model/internal/RawBoard.java
+++ src/main/java/org/shimomoto/mancala/model/internal/RawBoard.java
@@ -12,26 +12,29 @@ import java.util.Optional;
 @Builder(toBuilder = true)
 @Data
 public class RawBoard {
+	@SuppressWarnings("UnusedAssignment")
 	@Builder.Default
 	int turnCount = 1;
+	@SuppressWarnings("UnusedAssignment")
 	@Builder.Default
 	Player currentPlayer = Player.ONE;
+	@SuppressWarnings("UnusedAssignment")
 	@Builder.Default
 	int[] board = {6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0};
 
-	private static boolean isPassingTurn(int endsAt) {
+	private static boolean isPassingTurn(final int endsAt) {
 		return endsAt != 6;
 	}
 
-	private static boolean isCapture(int endsAt, int[] board) {
+	private static boolean isCapture(final int endsAt, final int[] board) {
 		return endsAt >= 0 && endsAt < 6 && board[endsAt] == 1;
 	}
 
-	public boolean isLegalMove(Player player, int position) {
-		if (currentPlayer == player) {
-			return position >= 0 && position < 6 && board[position] != 0;
+	public boolean isLegalMove(final Player player, final int position) {
+		if (currentPlayer != player) {
+			return false;
 		}
-		return false;
+		return position >= 0 && position < 6 && board[position] != 0;
 	}
 
 	public void changeTurn() {
@@ -43,7 +46,7 @@ public class RawBoard {
 		}
 	}
 
-	public void move(Player player, int position) {
+	public void move(final Player player, final int position) {
 		if (!isLegalMove(player, position)) {
 			return;
 		}
@@ -68,14 +71,14 @@ public class RawBoard {
 		turnCount++;
 	}
 
-	private void capture(int captureFrom) {
-		int opposingPosition = 12 - captureFrom;
+	private void capture(final int captureFrom) {
+		final int opposingPosition = 12 - captureFrom;
 		board[6] += (board[opposingPosition] + board[captureFrom]);
 		board[opposingPosition] = 0;
 		board[captureFrom] = 0;
 	}
 
-	private void sow(int position, int rounds, int spare) {
+	private void sow(final int position, final int rounds, int spare) {
 		board[position] = 0;
 		for (int i = 0; i < board.length; i++) {
 			board[i] += rounds;
